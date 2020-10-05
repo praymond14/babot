b450cf83eaf17393000fdacf2bfbabeb7980291e
Hank Hershberger <47288270+Aconka@users.noreply.github.com>
2020-10-05 01:43:02 -0400

    Update babot.js

--- babot.js
+++ babot.js
@@ -1,5 +1,5 @@
 var Discord = require('discord.js'); //discord module for interation with discord api
-var babadata = require('./babotdata.json');//baba configuration file
+var babadata = require('./babotdata.json'); //baba configuration file
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
@@ -15,30 +15,30 @@ bot.on('ready', function (evt)
 //stuff when message is recived.
 bot.on('message', message => 
 {
-	if(message.content.toLowerCase().includes('!baba') && !message.author.bot)//if message contains baba and is not from bot
+	if(message.content.toLowerCase().includes('!baba') && !message.author.bot) //if message contains baba and is not from bot
 	{
 		var text = 'BABA IS ADMIN';
-		if(message.content.toLowerCase().includes('help'))//reply with help text is baba help
+		if(message.content.toLowerCase().includes('help')) //reply with help text is baba help
 		{
 			text += '\n use !BABA password to get passwords for servers';
 		}
-		if(message.content.toLowerCase().includes('password'))//reply with password file string if baba password
+		if(message.content.toLowerCase().includes('password')) //reply with password file string if baba password
 		{
 			text += '\n' + babadata.pass;
 		}
 		message.channel.send(text);
 	}
-	if(message.content.toLowerCase().includes('!delete'))//code to del and move to log
+	if(message.content.toLowerCase().includes('!delete')) //code to del and move to log
 	{
-		if(message.member.roles.cache.has(babadata.adminid))//check if admin
+		if(message.member.roles.cache.has(babadata.adminid)) //check if admin
 		{
 			var message_id = message.content.replace(/\D/g,''); //get message id
-			var chanMap = message.guild.channels.cache;//get a map of the channelt in the guild
-			for(let [k, chan] of chanMap)//iterate through all the channels
+			var chanMap = message.guild.channels.cache; //get a map of the channelt in the guild
+			for(let [k, chan] of chanMap) //iterate through all the channels
 			{
-				if(chan.type == "text")//make sure the channel is a text channel
+				if(chan.type == "text") //make sure the channel is a text channel
 				{
-					chan.messages.fetch(message_id).then(message => deleteAndArchive(message)).catch(console.error);//try to get the message, if it exists call deleteAndArchive, otherwise catch the error
+					chan.messages.fetch(message_id).then(message => deleteAndArchive(message)).catch(console.error); //try to get the message, if it exists call deleteAndArchive, otherwise catch the error
 				}
 			}
 		}
@@ -49,18 +49,18 @@ bot.on('message', message =>
 function deleteAndArchive(msg)
 {
 	var hiddenChan = msg.guild.channels.cache.get(babadata.logchn); //gets the special archive channel
-	var usr = msg.author;//gets the user that sent the message
-	var savemsg = "This message sent by: <@" + usr + ">\n> "//sets the header of the message to mention the original poster
-	savemsg += msg.content;//insert the actual message below
-	var attch = msg.attachments;//get the attacments from the original message
+	var usr = msg.author; //gets the user that sent the message
+	var savemsg = "This message sent by: <@" + usr + ">\n> "; //sets the header of the message to mention the original poster
+	savemsg += msg.content; //insert the actual message below
+	var attch = msg.attachments; //get the attacments from the original message
 	var newAttch;
-	hiddenChan.send(savemsg,);//send the text
+	hiddenChan.send(savemsg,); //send the text
 	for(let [k, img] of attch)
 	{
-		newAttch = new Discord.MessageAttachment().setFile(img.url);//get images
-		hiddenChan.send('attachment :'+k,newAttch);//send images
+		newAttch = new Discord.MessageAttachment().setFile(img.url); //get images
+		hiddenChan.send('attachment :'+k,newAttch); //send images
 	}
-	msg.delete();//delete the original
+	msg.delete(); //delete the original
 }
 
 //not shure what this does also but it was in jeremy's code so
