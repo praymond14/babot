29771a7726c7f8862d2e0a9f257b2cbc367803a1
KittenMoon <srgriep@gmail.com>
2021-01-31 21:12:43 -0500

    Update babot.js

--- babot.js
+++ babot.js
@@ -145,7 +145,69 @@ bot.on('message', message =>
 	}
 });
 
+<<<<<<< Updated upstream
 async function tempoutput(msg, lp)  //temporary output function for testing
+=======
+//async function tempoutput(msg, lp)  //temporary output function for testing
+//{
+//	var t = "";
+//
+//	for (i = 0; i < lp.length; i++) 
+//	{
+//		t += lp[i] + "\n";
+//	}
+//
+//	msg.channel.send(t);
+//}
+async function setGrole(msg, rname) //creates role and sets users
+{
+	try 
+	{
+		var role = msg.guild.roles.cache.find(r => r.name === rname); //get the role if already made (in case of redundancy)
+	
+		if (role == null) //if null make new role
+		{
+			console.log("Creating Role + " + rname);
+
+			//create the role
+			await msg.guild.roles.create({
+				data: {
+				  name: rname
+				},
+				reason: 'bot do bot thing',
+			  }).catch(console.error);
+
+			role = msg.guild.roles.cache.find(r => r.name === rname); //set role to the role that was made
+		}
+
+		var reactMap = msg.reactions.cache; //get a map of the reactions
+		for(let [k, reee] of reactMap) //iterate through all the reactions
+		{
+			reee.users.fetch().then((users) => {
+				RoleAdd(msg, users, role); //call the dumb roll function to do the work (had to be done)
+			}).catch(console.error);
+		}
+		//create role with no permisions, gray color that can be @ by every one
+		//get user list from reacations
+		//give users role
+	} 
+	catch (error) 
+	{
+		console.log("nos"); //if error this goes
+	}
+}
+
+async function RoleAdd(msg, users, role) //dumb user thing because it is needed to work
+{
+	for(let [k, uboat] of users) //iterate through all the users
+	{
+		let mem = msg.guild.member(uboat); //check if user is memeber
+		mem.roles.add(role); //add role to user
+	}
+}
+
+async function setVote(msg) //reacts to message with 👍 and 👎 for votes
+>>>>>>> Stashed changes
 {
 	var t = "";
 
