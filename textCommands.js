fa3ef4d16f29233a7b0af5b1babfdf21ac634756
KittenMoon <47439334+KittenMoon@users.noreply.github.com>
2023-04-26 02:51:43 -0400

    anti-crash update

--- textCommands.js
+++ textCommands.js
@@ -202,10 +202,10 @@ function babaMessage(bot, message)
 			message.channel.send(babaJeremy());
 		}
 
-		if (msgContent.includes("cat"))
-		{
-			message.channel.send(babaCat());
-		}
+		// if (msgContent.includes("cat"))
+		// {
+		// 	message.channel.send(babaCat());
+		// }
 
 		if (msgContent.includes("weather"))
 		{
@@ -478,7 +478,6 @@ function babaMessage(bot, message)
 			var message_id = message.content.replace(role_name,''); //remove role name from string
 			message_id = message_id.replace(/\D/g,''); //get message id
 			var fnd = false;
-			console.log(role_name);
 			
 			var chanMap = message.guild.channels.fetch().then(channels => {
 				channels.each(chan => { //iterate through all the channels
